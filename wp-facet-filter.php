57ed90525ff733e9e41a21b82551f929ce0af77d
alexszeliga <alexszeliga@gmail.com>
2019-07-21 15:24:02 -0400

    added basic plugin file?

---
+++ wp-facet-filter.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Plugin Name: WP Facet Filter
+* Plugin URI: alexszeliga.com
+* Description: A plugin which filters your posts
+* Version: 0.1
+* Author: Alex Szeliga
+* Author URI: alexszeliga.com
+**/
+?>
